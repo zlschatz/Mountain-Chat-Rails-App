44d5819c0c72d6f65186fce5584b84ca5134f1c0
Zach Schatz <zachschatz@gmail.com>
2016-03-10 13:07:55 -0500

    Logic for user authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,23 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  helper_method :current_user, :logged_in?
+
+  def current_user
+    @user ||= User.find session[:user_id] if session[:user_id]
+  end
+
+  def authorize_user!
+    redirect_to new_admin_session_path unless current_user.present?
+  end
+
+  def login user
+    session[:user_id] = user.id
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
 end
