daa86461e70539075d7ef894850977aa9e47eaa9
Zach Schatz <zachschatz@gmail.com>
2016-03-15 15:56:22 -0400

    Valid format of email

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,6 @@ class User < ActiveRecord::Base
   validates :name, presence: true, length: {in: 8..20}
   validates :email, presence: true
 
+  validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
 
 end
