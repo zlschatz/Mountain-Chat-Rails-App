4582d32e22414b52b9a837ed34636914ea7f7804
Zach Schatz <zachschatz@gmail.com>
2016-03-15 14:38:37 -0400

    Define create route

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,25 @@
 class CommentsController < ApplicationController
 
   def new
-    # binding.pry
     @trail = Trail.find(params[:trail_id])
     @comments = @trail.comments
     render :new
+  end
+
+  def create
+    @comment = Comment.new(comment_params)
+    @comment.user_id = current_user
+    if @comment.save
+      redirect_to trail_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content,:user_id,:trail_id)
   end
 
 end
