7b19877d2b54ac4564ff7c2dcaf724638415bbb1
Zach Schatz <zachschatz@gmail.com>
2016-03-15 15:51:15 -0400

    Add user#create logic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,17 @@ class UsersController < ApplicationController
     @user = User.find_by(id: current_user.id)
   end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      login @user
+      redirect_to root_path
+    else
+      flash[:notice] = "Invalid email or password"
+      render new_user_path
+    end
+  end
+
   private
 
   def user_params
