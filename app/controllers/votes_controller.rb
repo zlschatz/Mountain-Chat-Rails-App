6475813bcc990376e3ee97110d7921f9545bb556
Zach Schatz <zachschatz@gmail.com>
2016-03-21 14:42:26 -0400

    Include logic for creating a vote

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,21 +1,15 @@
 class VotesController < ApplicationController
 
   def create
-    @vote = Vote.new(vote_params)
+    @vote = Vote.new(value: 1, comment_id: params[:comment_id], user_id: current_user.id)
     if @vote.save
-      redirect_to trail_path
+      redirect_to trail_path(@vote.comment.trail.id)
     else
-      redirect_to trail_path
+      redirect_to trail_path(@vote.comment.trail.id)
     end
   end
 
   def destroy
-  end
-
-  private
-
-  def vote_params
-    params.require(:vote).permit(:value, :user_id, :comment_id)
   end
 
 end
