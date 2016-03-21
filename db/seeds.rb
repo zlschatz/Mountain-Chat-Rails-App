36e90cb6366c5591bea55981682ee3973706933f
Zach Schatz <zachschatz@gmail.com>
2016-03-21 13:12:47 -0400

    Revise seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -31,6 +31,6 @@ trail_3 = Trail.create(name: "Trail 3", mountain_id: 1)
 trail_4 = Trail.create(name: "Trail 4", mountain_id: 1)
 
 Comment.create(content: "Sunny on the right side. Bumped on the left.", trail_id: 1, user_id: 1)
-Comment.create(content: "Dust over crust. Beware!", trail_id: trail_1, user_id: 2)
+Comment.create(content: "Dust over crust. Beware!", trail_id: 1, user_id: 2)
 Comment.create(content: "Stay close to the chair on skier's left. Fresh powder around the trees.", trail_id: 1, user_id: 3 )
 Comment.create(content: "Lost my glove on the slope if anyone grabs it, let me know!", trail_id: 1, user_id: 4)
