317e87309dbb3c6a16354be6c8eae90e542e290f
Zach Schatz <zachschatz@gmail.com>
2016-03-21 14:50:17 -0400

    Include logic to not allow multiple votes by one user on the same comment

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,8 +1,9 @@
 class VotesController < ApplicationController
 
   def create
+    @comment = Comment.find(params[:comment_id])
     @vote = Vote.new(value: 1, comment_id: params[:comment_id], user_id: current_user.id)
-    if @vote.save
+    if !@comment.votes.find_by(user_id: current_user.id) && @vote.save
       redirect_to trail_path(@vote.comment.trail.id)
     else
       redirect_to trail_path(@vote.comment.trail.id)
