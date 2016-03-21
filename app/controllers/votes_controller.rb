e1375a1a6e1a81ab89918f7f8a4b356193b7dc83
Zach Schatz <zachschatz@gmail.com>
2016-03-21 13:40:31 -0400

    Define vote creation

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,7 +1,12 @@
 class VotesController < ApplicationController
 
   def create
-
+    @vote = Vote.new(vote_params)
+    if @vote.save
+      redirect_to trail_path
+    else
+      redirect_to trail_path
+    end
   end
 
   def destroy
@@ -10,7 +15,7 @@ class VotesController < ApplicationController
   private
 
   def vote_params
-
+    params.require(:vote).permit(:value, :user_id, :comment_id)
   end
 
 end
