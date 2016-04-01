6300e1843fea933768482d5d894ddb034d978563
Zach Schatz <zachschatz@gmail.com>
2016-04-01 16:33:22 -0400

    Create ajax return for posting comment

--- app/assets/javascripts/mountain_chat.js
+++ app/assets/javascripts/mountain_chat.js
@@ -1,4 +1,34 @@
 $(document).on("ready", function() {
 
+  // $("a").click(".snowflake", function(event){
+  //   event.preventDefault();
+  //   event.stopPropagation();
+  //   debugger
+  //   // var target = $(event.target).parent().find("#vote_count")
+  //   $.ajax({
+  //     type: "POST",
+  //     url : $(this).attr('href'),
+  //     dataType: "json",
+  //   }).done(function(response){
+  //     debugger
+  //     alert("Success!")
+  //   }).fail(function(response){
+  //     console.log(response);
+  //   });
+  // });
+
+  $(".post-comment").on("click", function(event){
+    event.preventDefault();
+    // debugger;
+    $.ajax({
+      url : $(event.target).attr('href'),
+      data: $(this).serialize(),
+    }).done(function(response){
+      debugger;
+      console.log("success!");
+    }).fail(function(response){
+      alert("You are unable to post a comment.");
+    });
+  });
 
 });
