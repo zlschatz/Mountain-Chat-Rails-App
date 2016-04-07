c039c7500d7728fb8171f352f99166758b8d6144
Zach Schatz <zachschatz@gmail.com>
2016-04-07 17:07:49 -0400

    Comment out AJAX data calls

--- app/assets/javascripts/mountain_chat.js
+++ app/assets/javascripts/mountain_chat.js
@@ -1,5 +1,16 @@
 $(document).on("ready", function() {
 
+  $("#post").on("click", function(event){
+    event.preventDefault();
+    $.ajax({
+      url : $(event.target).attr('href'),
+    }).done(function(response){
+      $(".comment-form").show();
+    }).fail(function(response){
+      alert("You are unable to post a comment.");
+    });
+  });
+
   // $("a").click(".snowflake", function(event){
   //   event.preventDefault();
   //   event.stopPropagation();
@@ -17,32 +28,22 @@ $(document).on("ready", function() {
   //   });
   // });
 
-  $("#post").on("click", function(event){
-    event.preventDefault();
-    $.ajax({
-      url : $(event.target).attr('href'),
-    }).done(function(response){
-      $(".comment-form").show();
-    }).fail(function(response){
-      alert("You are unable to post a comment.");
-    });
-  });
-
-  $('.comment-form').on("submit", 'form', function(event){
-    event.preventDefault();
-    var id = $('#comment_trail_id').val()
-    $.ajax({
-      type: $(event.target).attr('method'),
-      url : '/trails/' + id + '/comments',
-      data: $(this).serialize()
-    }).done(function(response){
-      debugger;
-      $(".river").append(response);
-      $(".comment-form").remove();
-      $(".comment-form").hide();
-    }).fail(function(){
-      alert("Your post has not been posted")
-    });
-  });
+  // $('.comment-form').on("submit", 'form', function(event){
+  //   event.preventDefault();
+  //   var id = $('#comment_trail_id').val()
+  //   // debugger;
+  //   $.ajax({
+  //     type: $(event.target).attr('method'),
+  //     url : '/trails/' + id + '/comments',
+  //     data: $(this).serialize()
+  //   }).done(function(response){
+  //     debugger;
+  //     $(".river").toggle().toggle();
+  //     $(".comment-form").remove();
+  //     $(".comment-form").hide();
+  //   }).fail(function(){
+  //     alert("Your post has not been posted")
+  //   });
+  // });
 
 });
