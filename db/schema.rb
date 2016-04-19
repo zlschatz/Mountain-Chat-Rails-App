299b4970702352d96ec8ce812fc0f009c2717422
Zach Schatz <zachschatz@gmail.com>
2016-04-19 12:50:08 -0400

    Transition to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160309210348) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "comments", force: :cascade do |t|
     t.text     "content",    null: false
     t.integer  "user_id",    null: false
